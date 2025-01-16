b184af1ce906ff0d651ae6d53b0622e61a53894c
BVennela <105879812+BVennela@users.noreply.github.com>
2025-01-16 21:51:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browers
+
+console.log(3 
+    + 
+    3) // code readablitly should be high
+
+console.log("Vennela")
+
+
+let name = " vennela"
+let age = 13
+let isLoggedIn = false 
+let state;
+
+// number => 2 to power of 52 or 53 need to check 
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // output: undefined
+console.log(typeof null); //output: object
